109a969ba9244b0f2f182bd2592d6b251c529894
Emmanuel Kufre <whitemaxwell5@gmial.com>
2023-01-30 18:54:41 +0100

    working on seperating layouts for front end

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -1,17 +1,16 @@
-<x-app-layout>
-    <x-slot name="header">
-        <h2 class="font-semibold text-xl text-gray-800 leading-tight">
-            {{ __('Dashboard') }}
-        </h2>
-    </x-slot>
+@extends('layouts.custom.app')
 
-    <div class="py-12">
-        <div class="max-w-7xl mx-auto sm:px-6 lg:px-8">
-            <div class="bg-white overflow-hidden shadow-sm sm:rounded-lg">
-                <div class="p-6 bg-white border-b border-gray-200">
-                    
+@section('page_content')
+    
+                <!-- Begin Page Content -->
+                <div class="container-fluid">
+
+                    <!-- Page Heading -->
+                    <h1 class="h3 mb-4 text-gray-800">Blank Page</h1>
+
                 </div>
-            </div>
-        </div>
-    </div>
-</x-app-layout>
+                <!-- /.container-fluid -->
+
+          
+@endsection
+          
---
+++ resources/views/layouts/custom/js_scripts.blade.php
@@ -0,0 +1,9 @@
+    <!-- Bootstrap core JavaScript-->
+    <script src="{{asset('js/custom/vendor/jquery/jquery.min.js')}}"></script>
+    <script src="{{asset('js/custom/vendor/bootstrap/js/bootstrap.bundle.min.js')}}"></script>
+
+    <!-- Core plugin JavaScript-->
+    <script src="{{asset('js/custom/vendor/jquery-easing/jquery.easing.min.js')}}"></script>
+
+    <!-- Custom scripts for all pages-->
+    <script src="{{asset('js/custom/js/sb-admin-2.min.js')}}"></script>
