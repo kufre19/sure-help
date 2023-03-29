d6c19b15224016855566f79aa093414fd84380d4
Emmanuel Kufre <whitemaxwell5@gmial.com>
2023-03-29 22:35:33 +0100

    auto play fixed

--- resources/views/dashboard.blade.php
+++ resources/views/dashboard.blade.php
@@ -34,9 +34,9 @@
                             <div class="embed-responsive embed-responsive-16by9">
                                 <iframe class="embed-responsive-item" src="{{ $post->post_video }}"
                                     allow="autoplay; encrypted-media" allowfullscreen="allowfullscreen" frameborder="0"
-                                    autoplay="0" controls></iframe>
+                                    controls></iframe>
                             </div>
-
+                            
                             <p>{{ $post->post_description }}</p>
                         </div>
                         <div class="card-footer">
