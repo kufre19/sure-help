02f06be327550527ce9cac66223d6f911d951df4
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-12-04 13:49:10 +0100

    sal

--- routes/web.php
+++ routes/web.php
@@ -24,7 +24,7 @@ Route::get('/upload', function () {
 });
 
 Route::post('/upload', function (Request $request) {
-    $request->file('image')->move('uploads', 'public');
+    $request->file('image')->store('uploads', 'public');
     return back()->with('success', 'File has been uploaded.');
 });
 
