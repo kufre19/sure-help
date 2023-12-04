6f9d8cd51b56b7c37f38b7da0e5c8e8c7bd52b4c
Emmanuel kufre <whitemaxwell5@gmail.com>
2023-12-04 13:51:44 +0100

    sal

--- routes/web.php
+++ routes/web.php
@@ -19,6 +19,11 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get("info", function()
+{
+    return phpinfo();
+});
+
 Route::get('/upload', function () {
     return view('upload');
 });
